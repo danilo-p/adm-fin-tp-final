1e6b3a8c30611c3e515169604690c8c5ee99e7f7
Deheide <victor_hugocv@hotmail.com>
2020-10-29 14:35:44 -0300

    adding wacc formula in our calculator

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { ConversaoDeJurosComponent } from './conversao-de-juros/conversao-de-juros.component';
 import { Formula1Component } from './formula1/formula1.component';
 import { MeuComponenteComponent } from './meu-componente/meu-componente.component';
+import { WaccComponent } from './wacc/wacc.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ConversaoDeJurosComponent,
     Formula1Component,
-    MeuComponenteComponent
+    MeuComponenteComponent,
+    WaccComponent
   ],
   imports: [
     BrowserModule,
