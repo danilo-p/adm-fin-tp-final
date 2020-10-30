1107b5eb8608d6d7f13df249027c57ea499fdd3f
fredericoqueiroz <fredqueirozbh@gmail.com>
2020-10-30 11:50:47 -0300

    formulario PV funcional

--- src/app/time-value-money/time-value-money.component.ts
+++ src/app/time-value-money/time-value-money.component.ts
@@ -1,15 +1,27 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+// @ts-ignore
+import  Finance  from '../../../node_modules/tvm-financejs/';
 
 @Component({
   selector: 'app-time-value-money',
   templateUrl: './time-value-money.component.html',
   styleUrls: ['./time-value-money.component.css']
 })
-export class TimeValueMoneyComponent implements OnInit {
+export class TimeValueMoneyComponent {
 
-  constructor() { }
+  finance = new Finance();
 
-  ngOnInit(): void {
+  presentValue = 0;
+  futureValue = 0;
+  payments = 0;
+  numPeriods = 0;
+  rate = 0;
+  begin = 0;
+
+  tvmRadio = 0;
+
+  calculaValorPresente() {
+    this.presentValue = this.finance.PV(this.rate, this.numPeriods, this.payments, this.futureValue, this.begin)
   }
 
 }
